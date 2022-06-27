402b9f30c617aa4863a920c09771a933aa5b6425
Asaduzzaman Alamin <asaduzzamanrossow2000@gmail.com>
2022-06-27 22:12:08 +0600

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -18,8 +18,28 @@ async function run(){
     
     try{
         await client.connect();
-        const mobileCollection = client.db("mobiles").collection("data");
+        const mobileCollection = client.db("iphone").collection("data1");
         console.log('connected');
+
+
+
+        app.get('/iphones', async(req , res)=>{
+            const query = {};
+            const cursor = mobileCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+
+
+        app.get('/iphones/:id' , async(req , res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result =await mobileCollection.findOne(query);
+            res.send(result);
+
+        })
 
     }
     finally{
